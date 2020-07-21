7e9e1609778f6a9f591459f74674fa7a81587b45
csjiabin <qq1187712426@gmail.com>
2020-07-21 12:27:19 +0800

    webpack autoOpenBrowser

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -1,13 +1,15 @@
 "use strict";
 const { merge } = require("webpack-merge");
 const baseWebpackConfig = require("./webpack.base.conf");
-
+const OpenBrowserPlugin = require("open-browser-webpack-plugin");
+const port = process.env.PROT || 9000;
 module.exports = merge(baseWebpackConfig, {
   mode: "development",
   optimization: { minimize: false },
+  plugins: [new OpenBrowserPlugin({ url: `http://localhost:${port}` })],
   devServer: {
+    port,
     compress: true,
-    port: 9000,
     progress: true,
   },
 });
